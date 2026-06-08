778f468541108f5646b390f9656c3378344b71d8
Tesi <alessandrostuccio@gmail.com>
2026-06-08 16:20:43 +0200

    add util package

---
+++ kore/src/main/java/com/project/kore/util/BusinessConstants.java
@@ -0,0 +1,13 @@
+package com.project.kore.util;
+
+/**
+ * Costanti di business condivise tra i vari layer.
+ */
+public final class BusinessConstants {
+
+    private BusinessConstants() {}
+
+    public static final int MAX_CLIENTS_PER_PROFESSIONAL = 50;
+    public static final int MAX_MESSAGE_LENGTH = 2000;
+    public static final int MIN_PASSWORD_LENGTH = 8;
+}
